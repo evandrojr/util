b1b44a0dc29f182e5ad9488f2fdeb736043ee804
Evandro Junior <evandrojr@gmail.com>
2015-03-31 18:50:41 -0300

    nf

---
+++ noosfero/testes_sinteticos.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+# ARGV.each do|a|
+#   puts "Argument: #{a}"
+# end
+
+contents = File.read(ARGV[0])
+#contents = File.read("resultado")
+#puts contents
+
+tests = assertions = failures = errors = skips = 0
+
+contents.scan(/\d+ tests/) do |ds|
+  ds.scan(/\d+/) do |d|
+    tests+= d.to_i
+  end
+end
+
+contents.scan(/\d+ assertions/) do |ds|
+  ds.scan(/\d+/) do |d|
+    assertions+= d.to_i
+  end
+end
+
+contents.scan(/\d+ failures/) do |ds|
+  ds.scan(/\d+/) do |d|
+    failures+= d.to_i
+  end
+end
+
+contents.scan(/\d+ errors/) do |ds|
+  ds.scan(/\d+/) do |d|
+    errors+= d.to_i
+  end
+end
+
+contents.scan(/\d+ skips/) do |ds|
+  ds.scan(/\d+/) do |d|
+    skips+= d.to_i
+  end
+end
+
+puts "tests #{tests} assertions #{assertions} failures #{failures} errors #{errors} skips #{skips}"
