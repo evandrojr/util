777b42071b196bfd7a32cf3c55d219f05e30ce61
Evandro Junior <evandrojr@gmail.com>
2015-04-02 18:30:50 -0300

    fixed debug

--- noosfero/noosfero_files/debug.rb
+++ noosfero/noosfero_files/debug.rb
@@ -18,9 +18,19 @@ module Kernel
   def ra(object) 
     a = [] 
     a << "Should show: object.inspect, object.to_yaml, object.to_s"
+    a << "object.inspect =>"
     a << object.inspect
-    a << object.to_yaml
+    a << "**********************************************************"
+    a << "object.to_yaml =>"
+    begin  
+      a << object.to_yaml
+    rescue Exception => e  
+      a << "Erro no object.to_yaml " + e.message  
+    end      
+    a << "**********************************************************"
+    a << "object.to_s =>"
     a << object.to_s
+    a << "**********************************************************"
     raise a.inspect
   end
   
