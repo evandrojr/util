7114a313d31f554aabbbc6383f2a247423cd6156
Evandro Junior <evandrojr@gmail.com>
2015-05-08 11:43:51 -0300

    Added script to enable plugins in production

--- noosfero/copy_noosfero.sh
+++ noosfero/copy_noosfero.sh
@@ -4,4 +4,8 @@ cp -rv noosfero_files/nbproject ../../noosfero/
 cp -v noosfero_files/database.yml ../../noosfero/config/
 cp -v noosfero_files/debug.rb ../../noosfero/config/initializers/
 cp -v noosfero_files/ca ../../noosfero/script/ca
+cp -v noosfero_files/script/enable_participa_plugins ../../noosfero/script/
+
+#ci
 cp -v noosfero_files/ca ../../ci_noosfero/script/ca
+cp -v noosfero_files/script/enable_participa_plugins ../../ci_noosfero/script/
