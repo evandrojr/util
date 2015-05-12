eb20c47377282a2c2a1ca5f90dac6609d913b772
Evandro Junior <evandrojr@gmail.com>
2015-05-12 18:24:46 -0300

    added support for awesome_print

--- noosfero/copy_noosfero.sh
+++ noosfero/copy_noosfero.sh
@@ -3,13 +3,16 @@ cd $HOME/projetos/utils/noosfero
 cp -rv noosfero_files/nbproject ../../noosfero/
 cp -v noosfero_files/database.yml ../../noosfero/config/
 cp -v noosfero_files/debug.rb ../../noosfero/config/initializers/
-cp -v noosfero_files/ca ../../noosfero/script/ca
+cp -v noosfero_files/ca ../../noosfero/script/ca/
 cp -v noosfero_files/script/enable_participa_plugins ../../noosfero/script/
 
 #ci
-cp -v noosfero_files/ca ../../ci_noosfero/script/ca
+cp -v noosfero_files/ca ../../ci_noosfero/script/ca/
 cp -v noosfero_files/script/enable_participa_plugins ../../ci_noosfero/script/
 
 #RBenv
 cp -v noosfero_files/.rbenv-gemsets ../../noosfero/
 
+#awesome print plugin
+cp -rv noosfero_files/awesome_print ../../noosfero/plugins/
+cp -v noosfero_files/awesome_print.rb ../../noosfero/config/initializers/
---
+++ noosfero/noosfero_files/awesome_print/lib/awesome_print_plugin.rb
@@ -0,0 +1,13 @@
+class AwesomePrintPlugin < Noosfero::Plugin
+
+  def self.plugin_name
+    # FIXME
+    "AwesomePrintPlugin"
+  end
+
+  def self.plugin_description
+    # FIXME
+    _("A plugin that does this and that.")
+  end
+
+end
