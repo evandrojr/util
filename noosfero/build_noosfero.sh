dec37ce6792525f7b034bc521587c10b47730851
Evandro Jr <evandrojr@gmail.com>
2015-06-01 11:37:15 -0300

    fixes

--- noosfero/build_noosfero.sh
+++ noosfero/build_noosfero.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+echo "Atenção o script build_noosfero não apaga mais os arquivos não versionados, execute reset_noosfero.sh para isso"
 cd $HOME/projetos/util/noosfero
-./reset_noosfero.sh
+git clone git@gitlab.com:evandrojr/noosfero-plugins.git ~/projetos/noosfero/plugins/awesome_debug
 ./copy_noosfero.sh
--- noosfero/copy_noosfero.sh
+++ noosfero/copy_noosfero.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env sh
-cd $HOME/projetos/utils/noosfero
+cd $HOME/projetos/util/noosfero
 cp -rv noosfero_files/nbproject ../../noosfero/
 cp -v noosfero_files/database.yml ../../noosfero/config/
-cp -v noosfero_files/debug.rb ../../noosfero/config/initializers/
-cp -v noosfero_files/ca ../../noosfero/script/ca/
+cp -v noosfero_files/ca/* ../../noosfero/script/
+
+#plugings
 cp -v noosfero_files/script/enable_participa_plugins ../../noosfero/script/
 
 #ci
-cp -v noosfero_files/ca ../../ci_noosfero/script/ca/
-cp -v noosfero_files/script/enable_participa_plugins ../../ci_noosfero/script/
+cp -v noosfero_files/ca/* ../../ci_noosfero/script/
 
 #RBenv
 cp -v noosfero_files/.rbenv-gemsets ../../noosfero/
 
-#awesome print plugin
-cp -rv noosfero_files/awesome_print ../../noosfero/plugins/
-cp -v noosfero_files/awesome_print.rb ../../noosfero/config/initializers/
